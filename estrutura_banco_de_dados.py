6a55f838962c20f3808e2f84ebde77a5469e8919
Jefferson Santos <fshubble@gmail.com>
2024-07-15 15:37:33 -0300

    primeiro commit

---
+++ estrutura_banco_de_dados.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Criar um API flask
+app = Flask(__name__)
+
+
+
+# 1) - CRIANDO E CONFIGUANDO O BANCO DE DADOS !!!
+
+# Criar um instância de SQLAlchemy
+app.config['SECRET_KEY'] = 'FSD2323f#$!SAH'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:NXDpBWs06VcYHlaG@db.oyyodnntrigdmxgqydom.supabase.co:5432/postgres'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://usuario:senha@localhost:5432/nomedobanco'
+db = SQLAlchemy(app)
+db: SQLAlchemy
+
+
+# Definir a estrutra da tabela Postagem:
+#  id_postagem, titulo, autor
+class Postagem(db.Model):
+    __tablename__ = 'postagem'
+    id_postagem = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String)
+    id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor'))
+
+
+
+# Definir a estrutra da tabela Autor:
+#  id_autor, nome, email, senha, admin, postagens
+class Autor(db.Model):
+    __tablename__ = 'autor'
+    id_autor = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String)
+    email = db.Column(db.String)
+    senha = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+    postagens = db.relationship('Postagem')
+
+
+# 2) -INICIALIZANDO O BANCO DE DADOS!!
+def inicializar_banco():
+    with app.app_context():
+        # Executar o comando para criar o banco de dados
+        db.drop_all()
+        db.create_all()
+        # Criar usuários adminstradores
+        autor = Autor(nome='Jefferson Santos', email='fshubble@gmail.com',
+                    senha='#@123456#@', admin=True)
+        db.session.add(autor)
+        db.session.commit()
+
+
+if __name__ == "__main__":
+    inicializar_banco()
